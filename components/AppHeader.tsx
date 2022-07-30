996fc38883aebfb9c925a01dc9e8cafd708b8950
akrindev <akrinmin@gmail.com>
2022-07-30 17:20:40 +0700

    splite site logo components

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,20 +1,13 @@
 import Image from "next/image";
 import AppNavbar from "./AppNavbar";
+import AppSiteLogo from "./AppSiteLogo";
 
 export default function AppHeader() {
   return (
     <div className='fixed top-0 inset-x-0 bg-white'>
       <div className='mx-auto max-w-7xl'>
         <div className='px-5 lg:px-0 py-3 flex items-center justify-between '>
-          <div className='flex'>
-            <Image
-              src='/logo.png'
-              alt='smedip logo'
-              width={50}
-              height={50}
-              className='h-8 w-8'
-            />
-          </div>
+          <AppSiteLogo />
 
           <AppNavbar />
         </div>
---
+++ components/AppSiteLogo.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function AppSiteLogo() {
+  return (
+    <div className='flex items-center gap-6'>
+      <Image
+        src='/logo.png'
+        alt='smedip logo'
+        width={50}
+        height={50}
+        className='h-8 w-8'
+      />
+
+      <Link href={"/"}>
+        <a className='flex flex-col'>
+          <span className='font-bold text-xl border-b border-gray-600'>
+            SMK Diponegoro Karanganyar
+          </span>
+          <span>Kab. Pekalongan</span>
+        </a>
+      </Link>
+    </div>
+  );
+}
