c4e0446b7f44c836795f92065d38b35128a2602e
akrindev <akrinmin@gmail.com>
2022-07-31 11:41:24 +0700

    fixed responsive text size

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,18 +1,18 @@
 export default function HeroSection() {
   return (
     <div
-      className=' bg-white bg-cover'
+      className=' bg-white bg-cover bg-center'
       style={{
         backgroundImage: `url(/assets/images/bg-smedip.jpeg)`,
       }}>
       <div className='pt-16 mx-auto max-w-7xl'>
-        <div className='py-48 xl:py-56 px-5 xl:px-0 flex items-center'>
+        <div className='py-32 xl:py-56 px-5 xl:px-0 flex items-center'>
           <div className='flex flex-col text-white leading-tight drop-shadow-lg shadow-yellow-500'>
-            <span className='text-2xl'>Selamat datang di</span>
-            <span className='text-5xl font-extrabold underline'>
-              SMK Diponegoro Pekalongan
+            <span className='text-lg lg:text-2xl'>Selamat datang di</span>
+            <span className='text-2xl lg:text-5xl font-extrabold underline'>
+              SMK Diponegoro Karanganyar
             </span>
-            <span className='text-2xl'>Kabupaten Pekalongan</span>
+            <span className='text-lg lg:text-2xl'>Kabupaten Pekalongan</span>
           </div>
         </div>
       </div>
