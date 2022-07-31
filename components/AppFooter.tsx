f1cb3efb5cb92d7e2e6e58ec93d5d4d1ea73bb88
akrindev <akrinmin@gmail.com>
2022-07-31 12:42:15 +0700

    wip footer

---
+++ components/AppFooter.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
+export default function AppFooter() {
+  return (
+    <footer
+      className='mt-20 text-slate-100'
+      style={{
+        backgroundColor: `#081c05`,
+      }}>
+      <div className='pt-12 pb-10 '>
+        <div className='mx-auto max-w-7xl'>
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-12'>
+              <img
+                src='/logo.png'
+                alt='smk diponegoro pekalongan'
+                className='w-14 h-14 mb-5'
+              />
+            </div>
+            <div className='col-span-12 xl:col-span-4'>
+              <h1 className='mb-5 text-xl xl:text-2xl font-bold'>
+                SMK Diponegoro Karanganyar
+              </h1>
+              <p className='text-slate-400'>
+                VISI Terwujudnya Sekolah Sebagai Pusat Pendidikan dan Pelatihan
+                Kejuruan dengan Layanan Pendidikan yang Modern Sesuai
+                Perkembangan Ilmu Pengetahan yang Menghasilkan Sumber Daya
+                Manusia yang Unggul dalam Prestasi dan Berakhlak Islami (Modern
+                Agamis Unggul)
+              </p>
+            </div>
+
+            <div className='col-span-12 xl:col-span-4'>
+              <h1 className='text-xl xl:text-2xl font-bold'>
+                Kompetensi Keahlian
+              </h1>
+
+              <ul className='mt-5 text-slate-400'>
+                <li>
+                  <Link href={`#`}>
+                    <a className='hover:text-slate-100 hover:underline'>
+                      Teknik dan Bisnis Sepeda Motor
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href={`#`}>
+                    <a className='hover:text-slate-100 hover:underline'>
+                      Teknik Komputer dan Jaringan
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href={`#`}>
+                    <a className='hover:text-slate-100 hover:underline'>
+                      Agribisnis Tanaman Pangan dan Holtikultura
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href={`#`}>
+                    <a className='hover:text-slate-100 hover:underline'>
+                      Broadcasting dan Perfilman
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className='col-span-12 xl:col-span-4'>
+              <h1 className='text-xl xl:text-2xl font-bold mb-5'>
+                Lokasi Sekolah
+              </h1>
+              <address className='text-slate-400'>
+                Jl. Raya Karanganyar No.KM, RW.2, Sawah, Kayugeritan, Kec.
+                Karanganyar, Kabupaten Pekalongan, Jawa Tengah 51182
+              </address>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='py-5 text-center bg-black'>
+        &copy; {new Date().getFullYear()} SMK Diponegoro Karanganyar.
+      </div>
+    </footer>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import AppFooter from "../components/AppFooter";
 import AppLayout from "../components/layouts/AppLayout";
 import HeroSection from "../components/sections/HeroSection";
 import NewsSection from "../components/sections/NewsSection";
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <HeroSection />
       <NewsSection />
+      <AppFooter />
     </AppLayout>
   );
 };
