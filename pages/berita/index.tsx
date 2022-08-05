eedafea7fdac790a5c167fca6cbeb45e8fafa656
akrindev <akrinmin@gmail.com>
2022-08-05 08:14:48 +0700

    fixed showing index pages

--- pages/berita/index.tsx
+++ pages/berita/index.tsx
@@ -1,9 +1,11 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import AppLayout from "../../components/layouts/AppLayout";
 import NewsList from "../../components/NewsList";
 import Jumbotron from "../../components/sections/Jumbotron";
+import { getNews, News } from "../../lib/fetch";
 
-export default function Berita() {
+export default function Berita({ news }: { news: News[] }) {
   return (
     <AppLayout>
       <Head>
@@ -13,8 +15,18 @@ export default function Berita() {
       <Jumbotron title='Informasi Terbaru' />
 
       <div className='mx-auto max-w-6xl'>
-        <NewsList />
+        <NewsList list={news} />
       </div>
     </AppLayout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: news } = await getNews();
+
+  return {
+    props: {
+      news,
+    },
+  };
+};
--- pages/publikasi/index.tsx
+++ pages/publikasi/index.tsx
@@ -1,17 +1,33 @@
+import { GetStaticProps } from "next";
 import AppLayout from "../../components/layouts/AppLayout";
 import PublikasiList from "../../components/PublikasiList";
 import Jumbotron from "../../components/sections/Jumbotron";
+import { getPublikasi, Publikasi } from "../../lib/fetch";
 
-const Publikasi = () => {
+const Publikasi = ({ publications }: { publications: Publikasi[] }) => {
   return (
     <AppLayout>
       <Jumbotron title='Publikasi' />
 
       <div className='mx-auto max-w-6xl'>
-        <PublikasiList />
+        <div className='grid grid-cols-12'>
+          <div className='col-span-12 xl:col-span-8'>
+            <PublikasiList list={publications} />
+          </div>
+        </div>
       </div>
     </AppLayout>
   );
 };
 
 export default Publikasi;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: publications } = await getPublikasi();
+
+  return {
+    props: {
+      publications,
+    },
+  };
+};
