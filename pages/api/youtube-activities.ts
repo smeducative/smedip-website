b31c96f9fe6464fa510556418029717b064427b3
akrindev <akrinmin@gmail.com>
2022-08-22 09:42:20 +0700

    added yt feed

---
+++ pages/api/youtube-activities.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getActivities } from "../../lib/getYtActivities";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { items } = await getActivities();
+
+    // cache result for 1 hour
+    res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
+
+    res.status(200).json(items);
+  } catch (e) {
+    console.log(e);
+  }
+}
---
+++ type/youtube.ts
@@ -0,0 +1,63 @@
+export interface Activities {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  contentDetails: ContentDetails;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  type: string;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium: Medium;
+  high: High;
+  standard: Standard;
+  maxres: Maxres;
+}
+
+export interface Default {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Medium {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface High {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Standard {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Maxres {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface ContentDetails {
+  upload: Upload;
+}
+
+export interface Upload {
+  videoId: string;
+}
