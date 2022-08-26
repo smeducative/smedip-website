ead37ff3a8796b3c016a53b92a7cc1cadeedcd25
akrindev <akrinmin@gmail.com>
2022-08-26 12:19:25 +0700

    fixed in collaboration section

--- components/InCollaborateWith.tsx
+++ components/InCollaborateWith.tsx
@@ -1,38 +1,40 @@
-import Image from 'next/image'
-import smeducative from '../assets/collaborator/smeducative.png'
-import artv from '../assets/collaborator/artv.png'
+import Image from "next/image";
 
 const InCollaborateWith = () => {
-    const collaborators = [
-        {
-            name: 'Smeducative',
-            logo: smeducative
-        }, {
-            name: 'AR TV',
-            logo: artv
-        }
-    ]
+  const collaborators = [
+    {
+      name: "Smeducative",
+      logo: "/assets/collaborator/smeducative.png",
+    },
+    {
+      name: "AR TV",
+      logo: "/assets/collaborator/artv.png",
+    },
+    {
+      name: "creasico",
+      logo: "/assets/collaborator/creasico.png",
+    },
+  ];
 
-
-    return <div className="mx-auto max-w-6xl">
-
-        <div className="py-12 px-5 xl:px-0">
-            <div className="flex flex-wrap">
-                {collaborators.map(collaborator => (
-                    <Image 
-                        key={collaborator.name}
-                        src={collaborator.logo}
-                        alt={collaborator.name}
-                        height={120}
-                        width={'100%'}
-                        className={`obje`}
-                    />
-                ))}
-            </div>
+  return (
+    <div className='mt-12 mx-auto max-w-6xl'>
+      <div className='py-12 px-5 xl:px-0'>
+        <div className='flex items-center justify-center textlg xl:text-2xl font-bold font-roboto'>
+          In Collaboration with
         </div>
-
+        <div className='flex flex-row flex-wrap gap-6 items-center justify-center'>
+          {collaborators.map((collaborator) => (
+            <img
+              key={collaborator.name}
+              src={collaborator.logo}
+              alt={collaborator.name}
+              className={`h-32 xl:h-44 w-32 xl:w-44 object-contain`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
-
-export default InCollaborateWith
+export default InCollaborateWith;
