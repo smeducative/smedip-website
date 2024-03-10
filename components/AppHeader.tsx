dba7c24f1dfd1343a71dec76871277a4bc689575
Syakirin Amin <akrinmin@gmail.com>
2024-03-10 10:37:46 +0700

    fix: upgrade
    Signed-off-by: Syakirin Amin <akrinmin@gmail.com>

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -7,27 +7,30 @@ export default function AppHeader() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='fixed top-0 inset-x-0 z-50 bg-white shadow shadow-green-200 bg-opacity-75 filter backdrop-blur-md border-t-4 border-primary-0'>
-      <div className='mx-auto max-w-7xl'>
-        <div className='px-5 py-3 flex items-center justify-between'>
+    <div className="fixed top-0 inset-x-0 z-50 bg-white shadow shadow-green-200 bg-opacity-75 filter backdrop-blur-md border-t-4 border-primary-0">
+      <div className="mx-auto max-w-7xl">
+        <div className="px-5 py-3 flex items-center justify-between">
           <AppSiteLogo />
 
           <AppNavbar />
 
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className='block lg:hidden'>
+            className="block lg:hidden"
+          >
             <svg
-              className='w-6 h-6'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'>
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h16m-7 6h7'></path>
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
             </svg>
           </button>
         </div>
