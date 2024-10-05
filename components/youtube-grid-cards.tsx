e1f115d8c248acf80e863d9f397d92637ae2413f
Syakirin Amin <akrinmin@gmail.com>
2024-10-05 17:59:27 +0700

    fix: yt

--- components/youtube-grid-cards.tsx
+++ components/youtube-grid-cards.tsx
@@ -3,6 +3,7 @@ import { Play } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import { Skeleton } from "./ui/skeleton";
 
 export type YTSnippet = {
   publishedAt: string;
@@ -63,16 +64,16 @@ export type YTVideo = {
 
 export default function YoutubeGridCards() {
   const [videos, setVideos] = useState<YTVideo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchVideos = async () => {
-      const smedipYT = await fetch(
-        "/api/youtube-activities?channelId=UCtbl00zVFRkH2cALJgSN3Uw"
-      );
+      setIsLoading(true);
 
-      const bdpTV = await fetch(
-        "/api/youtube-activities?channelId=UCZ5gDURHX02514KEbzt6sVQ"
-      );
+      const [smedipYT, bdpTV] = await Promise.all([
+        fetch("/api/youtube-activities?channelId=UCtbl00zVFRkH2cALJgSN3Uw"),
+        fetch("/api/youtube-activities?channelId=UCZ5gDURHX02514KEbzt6sVQ"),
+      ]);
 
       const data = await smedipYT.json();
 
@@ -87,6 +88,7 @@ export default function YoutubeGridCards() {
       );
 
       setVideos(sorted);
+      setIsLoading(false);
     };
 
     fetchVideos();
@@ -108,43 +110,53 @@ export default function YoutubeGridCards() {
         viewport={{ once: true }}
         transition={{ duration: 1.5 }}
         className='gap-4 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
-        {videos.map((video, index) => (
-          <motion.div
-            key={video.id}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 1.5, delay: index * 0.2 }}>
-            <Card className='rounded-md overflow-hidden'>
-              <Link
-                // will tsrget="_blank" to open in new tab
-                href={`https://www.youtube.com/watch?v=${video.contentDetails.upload.videoId}`}
-                target='_blank'>
-                <div className='relative cursor-pointer aspect-video'>
-                  <img
-                    alt={video.snippet.title}
-                    className='w-full h-full object-cover'
-                    height='180'
-                    src={video.snippet.thumbnails.high.url}
-                    style={{
-                      aspectRatio: "16/9",
-                      objectFit: "cover",
-                    }}
-                    width='320'
-                  />
-                  <div className='absolute inset-0 flex justify-center items-center bg-black bg-opacity-50'>
-                    <Play className='opacity-80 w-12 h-12 text-white' />
+        {isLoading && (
+          <div className='gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+            {[1, 2, 3, 4, 5, 6].map((i) => (
+              <Card key={i} className='rounded-md overflow-hidden'>
+                <Skeleton className='w-full h-12 object-cover' />
+              </Card>
+            ))}
+          </div>
+        )}
+        {isLoading === false &&
+          videos.map((video, index) => (
+            <motion.div
+              key={video.id}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 1.5, delay: index * 0.2 }}>
+              <Card className='rounded-md overflow-hidden'>
+                <Link
+                  // will tsrget="_blank" to open in new tab
+                  href={`https://www.youtube.com/watch?v=${video.contentDetails.upload.videoId}`}
+                  target='_blank'>
+                  <div className='relative cursor-pointer aspect-video'>
+                    <img
+                      alt={video.snippet.title}
+                      className='w-full h-full object-cover'
+                      height='180'
+                      src={video.snippet.thumbnails.high.url}
+                      style={{
+                        aspectRatio: "16/9",
+                        objectFit: "cover",
+                      }}
+                      width='320'
+                    />
+                    <div className='absolute inset-0 flex justify-center items-center bg-black bg-opacity-50'>
+                      <Play className='opacity-80 w-12 h-12 text-white' />
+                    </div>
                   </div>
-                </div>
-              </Link>
-              <CardContent className='p-4'>
-                <h2 className='line-clamp-2 font-semibold text-sm'>
-                  {video.snippet.title}
-                </h2>
-              </CardContent>
-            </Card>
-          </motion.div>
-        ))}
+                </Link>
+                <CardContent className='p-4'>
+                  <h2 className='line-clamp-2 font-semibold text-sm'>
+                    {video.snippet.title}
+                  </h2>
+                </CardContent>
+              </Card>
+            </motion.div>
+          ))}
       </motion.div>
     </motion.div>
   );
